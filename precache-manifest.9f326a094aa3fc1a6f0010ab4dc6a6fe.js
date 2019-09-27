2d7003c226d659e462a310a5f1d0b5ae8e062890
Cole Holmes <mitchellcholmes@gmail.com>
2019-09-27 13:14:33 -0500

    Updates

---
+++ precache-manifest.9f326a094aa3fc1a6f0010ab4dc6a6fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32b829324756893ddf11a92d216b4eb",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "41ef557ce20c40bfb9c2",
+    "url": "/project-4-assessment/static/css/main.abddb58e.chunk.css"
+  },
+  {
+    "revision": "0cc549c3b5dc81b00661",
+    "url": "/project-4-assessment/static/js/2.19100ec5.chunk.js"
+  },
+  {
+    "revision": "41ef557ce20c40bfb9c2",
+    "url": "/project-4-assessment/static/js/main.50913bd5.chunk.js"
+  },
+  {
+    "revision": "866df269a8473c6760aa",
+    "url": "/project-4-assessment/static/js/runtime-main.4e0c7202.js"
+  }
+]);
